e85a05a46e4839475bbb494822a611084748b936
deenos25 <deenos25@tamu.edu>
2025-09-15 22:39:26 -0500

    test that files are downloaded and add dataset readme

--- SAR/test_sar.py
+++ SAR/test_sar.py
@@ -1,1 +1,7 @@
 # https://github.com/benjaminlewis-afrl/SAMPLE_dataset_public
+import cv2
+image_path = "data/SAR/real/t72/t72_real_A_elevDeg_016_azCenter_013_77_serial_812.png"
+img = cv2.imread(image_path)
+cv2.imshow("Tank Image", img)
+cv2.waitKey(0)
+print(img.shape)
---
+++ data/README.md
@@ -0,0 +1,2 @@
+# RF-Practice/data
+SAR dataset from https://github.com/benjaminlewis-afrl/SAMPLE_dataset_public/tree/master/png_images/decibel
